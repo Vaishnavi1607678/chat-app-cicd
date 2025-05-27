c8f48b246001b55a603b6f596571f911f8e64012
vaishnavitikke <vaishnavitikke@gmail.com>
2025-05-27 22:04:23 +0530

    Initial commit with chat app and CI/CD

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// WebSocket connection
+io.on("connection", (socket) => {
+  console.log("User connected...");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
